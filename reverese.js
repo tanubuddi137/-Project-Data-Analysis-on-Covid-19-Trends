1a70b67073cb666323bc307c54dd621eaa37f840
Ashok137 <ashok.tanubuddi@gmail.com>
2022-04-05 13:51:17 +0200

    protype chain

---
+++ reverese.js
@@ -0,0 +1,20 @@
+// Prototype chain
+
+var obj1 = {
+    name: 'obj1',
+    value: 1
+};  
+var obj2 = {
+    name: 'obj2',
+    value: 2
+};
+var obj3 = {
+    name: 'obj3',
+    value: 3
+};
+
+console.log(obj1.name);
+console.log(obj2.name);
+console.log(obj3.name);
+
+
